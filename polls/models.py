a66dc80ddee20a9db98522bd5996c50e8e286dd4
Marike <marike.puranen@laura.fi>
2026-03-01 22:36:36 +0200

    Add owner to questions

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,13 @@
 import datetime
 
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.question_text
